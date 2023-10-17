745e565f718bea17de092e48604aec95482aa40c
Nikita <nikita.yaroshevich@gmail.com>
2023-10-17 22:52:54 +0100

    (hotfix) TS7053: Element implicitly has an  any  type
    Specify some types to fix "TS7053: Element implicitly has an  any  type because expression of type  string  can't be used to index type  {} 
    No index signature with a parameter of type  string  was found on type  {} "

--- packages/generator/src/helpers/getImplicitlyTypedHandlerCode.ts
+++ packages/generator/src/helpers/getImplicitlyTypedHandlerCode.ts
@@ -3,10 +3,10 @@ import { getModelFieldsVariableName } from "./common/names"
 
 export const getImplicitlyTypedHandlerCode = (model: DMMF.Model) => {
   return `export const polish${model.name} = <T>(input: T): T => {
-      const result = {};
+      const result:Record<string, any> = {};
   
       ${getModelFieldsVariableName(model.name)}.forEach((key) => {
-        result[key] = input[key];
+        result[key] = (input as any)[key];
       })
   
       return result as T;
